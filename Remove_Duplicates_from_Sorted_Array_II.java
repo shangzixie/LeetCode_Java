195ce0411220db6761f9e27ff3af944e55a6781f
shangzixie <xieshangzi@gmail.com>
2020-03-17 00:35:08 -0400

    03/17/2020

---
+++ Remove_Duplicates_from_Sorted_Array_II.java
@@ -0,0 +1,31 @@
+package LeetCode_Java;
+
+import java.util.Arrays;
+
+public class Remove_Duplicates_from_Sorted_Array_II {
+    public static int removeDuplicates(int[] nums) {
+        // edge case
+        if (nums.length <= 2) return nums.length;
+
+        int count = 1, slow = 0, fast = 1;
+        for (fast= 0; fast < nums.length; fast++){
+            if (fast >= 1 && nums[fast] == nums[fast-1]){
+                count += 1;
+            }else{
+                count = 1;
+            }
+            if (count <=2){
+                nums[slow] = nums[fast];
+                slow += 1;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+        return slow ;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,1,1,2,2,3};
+        System.out.println(removeDuplicates(nums));
+
+    }
+}
