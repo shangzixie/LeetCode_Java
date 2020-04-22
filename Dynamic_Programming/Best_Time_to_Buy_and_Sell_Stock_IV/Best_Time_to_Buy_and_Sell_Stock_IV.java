e3544ce7a7e7a886d5192e38fd8dc0a819600d8d
shangzixie <459079554@qq.com>
2020-04-22 00:16:52 -0700

    04/22/2020

---
+++ Dynamic_Programming/Best_Time_to_Buy_and_Sell_Stock_IV/Best_Time_to_Buy_and_Sell_Stock_IV.java
@@ -0,0 +1,43 @@
+package LeetCode_Java.Dynamic_Programming.Best_Time_to_Buy_and_Sell_Stock_IV;
+
+public class Best_Time_to_Buy_and_Sell_Stock_IV {
+
+    public int maxProfit(int K, int[] prices) {
+        int n = prices.length;
+
+        //if k means infinite number
+        if (K / 2 >= n){
+            int diff = 0;
+            int ans = 0;
+            for (int i =1; i < n; i++){
+                diff = prices[i] - prices[i-1];
+                if (diff> 0){
+                    ans += diff;
+                }
+            }
+            return ans;
+        }
+
+        int[] hold = new int[K+1];
+        int[] sold = new int[K+1];
+        hold[0] = 0;
+        sold[0] = 0;
+        for (int i = 0 ; i < n; i++){
+            int holdOld = hold;
+            int soldOld = sold;
+
+            for (int j = 1; j <= K; j++){
+                hold[j] = Math.max(holdOld[j], soldOld[j-1] - prices[i]);
+                sold[j] = Math.max(soldOld[j], holdOld[j] + prices[i]);
+            }
+        }
+
+        int result = Integer.MIN_VALUE;
+        for(int j = 0; j <= K; j++){
+            result = Math.max(result, sold[j]);
+        }
+
+        return result;
+
+    }
+}
---
+++ Math/Rotate_Array/Rotate_Array.java
@@ -0,0 +1,24 @@
+package LeetCode_Java.Math.Rotate_Array;
+
+public class Rotate_Array {
+    public static int[] rotate(int[] nums, int k) {
+
+
+        int i = 0;
+        int det = k % nums.length;
+        int n = nums.length;
+
+        while(i != 0){
+            int nextNum = nums[i + det % n];
+            nums[i+ det % n] = nums[i];
+            i += det % n;
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3,4,5};
+        int k = 3;
+        rotate(nums, k);
+    }
+}
