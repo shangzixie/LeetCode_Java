e8e4b56a44a7e1aa24c467bb30849af8c7410338
shangzixie <xieshangzi@gmail.com>
2021-10-22 01:01:05 +0800

    LRU

--- DataSturcture/LRU_Cache/LRU.js
+++ DataSturcture/LRU_Cache/LRU.js
@@ -3,103 +3,84 @@ var Node = function(key, value) {
   this.val = value;
   this.prev = this.next = null;
 }
-/**
-* DoublyLinkedList Class
-* @constructor initilize head & tail
-* @action insertHead
-* @action removeNode
-* @action moveToHead
-* @action removeTail
-*/
+
 var DoublyLinkedList = function() {
-  this.head = new Node;
-  this.tail = new Node;
-  this.head.next = this.tail;
-  this.tail.prev = this.head;
+    this.dummy = new Node();
+    this.tail = new Node();
+    this.dummy.next = this.tail;
+    this.tail.prev = this.dummy;
+}
+
+DoublyLinkedList.prototype.addToTail = function(node) {
+    const preNode = this.tail.prev;
+    preNode.next = node;
+    node.prev = preNode;
+
+    this.tail.prev = node;
+    node.next = this.tail;
+}
+
+DoublyLinkedList.prototype.moveToTail = function(node) {
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+    this.addToTail(node);
+}
+
+DoublyLinkedList.prototype.deleteHead = function() {
+    let nodeRemoved = this.dummy.next;
+    let newHead = nodeRemoved.next;
+    this.dummy.next = newHead;
+    newHead.prev = this.dummy;
+
+    return nodeRemoved;
 }
 
 /**
-* Insert a new node right after head
-* @param {Node} node
-*/
-DoublyLinkedList.prototype.insertHead = function(node) {
-  node.prev = this.head;
-  node.next = this.head.next;
-  this.head.next.prev = node;
-  this.head.next = node;
-}
-
-/**
-* Remove a node from the linked list
-* @param {Node} node
-*/
-DoublyLinkedList.prototype.removeNode = function(node) {
-  // console.log("trying to remove node:", node.key)
-  let prev = node.prev;
-  let next = node.next;
-  prev.next = next;
-  next.prev = prev;
-}
-
-/**
-* Move a node to the head
-* @param {Node} node
-*/
-DoublyLinkedList.prototype.moveToHead = function(node){
-  this.removeNode(node);
-  this.insertHead(node);
-}
-
-/**
-* Remove the tail element and return its key
-* @return {String}
-*/
-DoublyLinkedList.prototype.removeTail = function() {
-  let tail = this.tail.prev;
-  this.removeNode(tail);
-  return tail.key;
-}
-
-/**
-* @param {number} capacity
-*/
+ * @param {number} capacity
+ */
 var LRUCache = function(capacity) {
-  this.capacity = capacity;
-  this.currentSize = 0;
-  this.hash = new Map();
-  this.dll = new DoublyLinkedList();
+    this.capacity = capacity;
+    this.map = new Map();
+    this.size = 0;
+    this.dll = new DoublyLinkedList();
 };
 
 /**
-* @param {number} key
-* @return {number}
-*/
+ * @param {number} key
+ * @return {number}
+ */
 LRUCache.prototype.get = function(key) {
-  let node = this.hash.get(key);
-  if (!node) return -1;
-  this.dll.moveToHead(node);
-  return node.val;
+    if (!this.map.get(key)) return -1;
+
+    this.dll.moveToTail(this.map.get(key))
+    return this.map.get(key).val;
 };
 
 /**
-* @param {number} key
-* @param {number} value
-* @return {void}
-*/
+ * @param {number} key
+ * @param {number} value
+ * @return {void}
+ */
 LRUCache.prototype.put = function(key, value) {
-  let node = this.hash.get(key);
-  if (node==null) { // new node
-      let newNode = new Node(key, value);
-      this.hash.set(key, newNode);
-      this.dll.insertHead(newNode);
-      this.currentSize++;
-      if (this.currentSize > this.capacity) {
-          let tailKey = this.dll.removeTail();
-          this.hash.delete(tailKey);
-          this.currentSize--;
-      }
-  } else { // existed node, update its value and move to head;
-      node.val = value;
-      this.dll.moveToHead(node);
-  }
+    if(!this.map.has(key)) {
+        this.size += 1;
+        let node = new Node(key, value);
+        this.map.set(key, node);
+        this.dll.addToTail(node);
+
+        if (this.size > this.capacity) {
+            this.map.delete(this.dll.deleteHead().key);
+        }
+        return;
+    }
+    this.map.get(key).val = value;
+    this.dll.moveToTail(this.map.get(key));
 };
+
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
