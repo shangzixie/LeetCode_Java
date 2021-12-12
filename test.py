ef149128add03250705a57579b7aeddf150c8858
shangzixie <xieshangzi@gmail.com>
2021-12-12 21:48:30 +0800

    update test

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+'''
+maxHeap
+arr
+'''
 class Solution:
-    def pruneTree(self, gas: List, cost: List) -> None:
-        profit = 0
-
-        for i in range(len(gas)):
-            for j in range(i + 1, len(gas) + i):
-                index = j % len(gas)
-                profit += profit + gas[index] - cost[index]
-                if profit < 0:
-                    profix = 0
-                    break
+    self.heap = []
+    self.arr = []
+    def getKth(self, k) -> None:
+        num = None
+        while k > 0:
+            num = heapq.heappop(self.heap)
+            self.arr.append(num)
+            k-= 1
+        for i in range(len(arr)):
+            heapq.heappush(self.heap, arr[i])
+            self.arr = []
+            return num
---
+++ test.ts
@@ -0,0 +1,12 @@
+class Solution {
+    private solution() {
+
+    }
+}
+
+'AAA = 1 * 26^0 + 1 * 26^1 + 1 * 26^2'
+'ZZZ'
+
+'703 -> AAA'
+zA 26 * 26^1
+AZ
